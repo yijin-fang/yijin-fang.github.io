7b200746f0be90f0224c9e73c68c99f1574cdc4b
yijinfang <fangyijin2021@gmail.com>
2026-01-14 20:28:10 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Yijin's Homepage",
+    pageTitle: "Yijin's Home",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
